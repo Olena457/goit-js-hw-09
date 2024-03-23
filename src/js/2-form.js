69b63892adc09f83709e20f049067cbdf4338ae0
Olena457 <olenademianenko42@gmail.com>
2024-03-23 17:25:38 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,3 @@
-import validator from 'validator';
-
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
@@ -7,16 +5,13 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', onTextAreaInput);
 form.addEventListener('submit', onFormSubmit);
 
-let dataUserForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let dataUserForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 const { email, message } = form.elements;
 
-const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-function reoloadPage() {
-  if (savedData) {
-    email.value = savedData.email || '';
-    message.value = savedData.message || '';
-    dataUserForm = savedData;
+function reloadPage() {
+  if (dataUserForm) {
+    email.value = dataUserForm.email || '';
+    message.value = dataUserForm.message || '';
   }
 }
 
@@ -30,17 +25,18 @@ function onFormSubmit(e) {
   const trimmedEmail = email.value.trim();
   const trimmedMessage = message.value.trim();
 
-  if (!trimmedEmail || !trimmedMessage) {
+  if (trimmedEmail === '' || trimmedMessage === '') {
     return alert(`Будь ласка, заповніть всі обов'язкові поля`);
   }
 
-  if (!validator.isEmail(trimmedEmail)) {
-    return alert(`Будь ласка, введіть дійсну електронну адресу`);
-  }
+  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!emailRegex.test(trimmedEmail)) {
+  //   return alert(`Будь ласка, введіть дійсну електронну адресу`);
+  // }
 
   console.log('email:', trimmedEmail, 'massage:', trimmedMessage);
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
   dataUserForm = {};
 }
-reoloadPage();
+reloadPage();
