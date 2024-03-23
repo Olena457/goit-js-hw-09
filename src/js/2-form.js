ec6cf4df553b960d6e0ca2450a631ed8958bfdd2
Olena457 <olenademianenko42@gmail.com>
2024-03-23 08:10:44 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,5 @@
 import validator from 'validator';
 
-console.log(
-  'Is mango@mail.com a valid email?: ',
-  validator.isEmail('mango@mail.com')
-);
-console.log(
-  'Is Mangodogmail.com a valid email?: ',
-  validator.isEmail('Mangodogmail.com')
-);
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
@@ -15,29 +7,38 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', onTextAreaInput);
 form.addEventListener('submit', onFormSubmit);
 
-let dataUserForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+let dataUserForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
 const { email, message } = form.elements;
 
-function onTextAreaInput(e) {
-  dataUserForm = { email: email.value.trim(), message: message.value.trim() };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataUserForm).trim());
-}
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
 function reoloadPage() {
-  if (dataUserForm) {
-    email.value = dataUserForm.email || '';
-    message.value = dataUserForm.message || '';
+  if (savedData) {
+    email.value = savedData.email || '';
+    message.value = savedData.message || '';
+    dataUserForm = savedData;
   }
+}
+
+function onTextAreaInput(e) {
+  dataUserForm = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataUserForm));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log({ email: email.value, message: message.value });
+  const trimmedEmail = email.value.trim();
+  const trimmedMessage = message.value.trim();
 
-  if (email.value === '' || message.value === '') {
-    return alert(`Будь ласка, заповніть всі поля`);
+  if (!trimmedEmail || !trimmedMessage) {
+    return alert(`Будь ласка, заповніть всі обов'язкові поля`);
   }
 
+  if (!validator.isEmail(trimmedEmail)) {
+    return alert(`Будь ласка, введіть дійсну електронну адресу`);
+  }
+
+  console.log('email:', trimmedEmail, 'massage:', trimmedMessage);
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
   dataUserForm = {};
